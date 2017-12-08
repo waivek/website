529ceb1bf6e3bb7a8332bc13645e0c8a77579447
Vivek Bose <waivek@gmail.com>
2017-12-08 17:26:43 +0530

    Initial Commit - Added JS, CSS

---
+++ js/background.js
@@ -0,0 +1,12 @@
+var Dark = function()
+{
+    document.getElementsByClassName("dark_stylesheet_link")[0].href="css_dir_link/dark.css";
+
+
+};
+
+var Light = function()
+{
+    document.getElementsByClassName("dark_stylesheet_link")[0].href="";
+
+}
