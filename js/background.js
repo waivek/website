87aa868afb2ebfc0b60c377b0c6661ada4ae9e88
Vivek Bose <waivek@gmail.com>
2017-12-10 22:11:12 +0530

    Merged dark and light stylesheets. Eliminated XHR/additional requests

--- js/background.js
+++ js/background.js
@@ -1,29 +1,11 @@
 var Dark = function()
 {
-    document.getElementsByClassName("dark_stylesheet_link")[0].href="css/dark.css";
-
-
+    document.body.className = "dark";
 };
 
 var Light = function()
 {
-    document.getElementsByClassName("dark_stylesheet_link")[0].href="";
-
-};
-var DarkStylesheetRequestCompleted = function () {
-    // console.log(httpRequest.readyState);
-    // console.log(XMLHttpRequest.DONE);
-    console.log("Request completed");
-};
-
-var LoadStylesheet = function() {
-    var httpRequest;
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange =  DarkStylesheetRequestCompleted;
-    httpRequest.open('GET', 'css/dark.css');
-    httpRequest.send();
-
+    document.body.className = "";
 
 };
 
-LoadStylesheet();
