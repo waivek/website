7bd8867b308944c19cec74a42df5ae1a1ff323a3
Vivek Bose <waivek@gmail.com>
2017-12-10 21:19:06 +0530

    Trying AJAX on site

--- js/background.js
+++ js/background.js
@@ -9,4 +9,21 @@ var Light = function()
 {
     document.getElementsByClassName("dark_stylesheet_link")[0].href="";
 
-}
+};
+var DarkStylesheetRequestCompleted = function () {
+    // console.log(httpRequest.readyState);
+    // console.log(XMLHttpRequest.DONE);
+    console.log("Request completed");
+};
+
+var LoadStylesheet = function() {
+    var httpRequest;
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange =  DarkStylesheetRequestCompleted;
+    httpRequest.open('GET', '/css/dark.css');
+    httpRequest.send();
+
+
+};
+
+// LoadStylesheet();
