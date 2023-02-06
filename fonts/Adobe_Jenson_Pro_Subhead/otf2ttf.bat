c735f85fc8fc4ed8815dac6c5161aa044a6edf76
Vivek Bose <waivek@gmail.com>
2023-02-06 05:32:31 +0530

    (auto-commit)

---
+++ fonts/Adobe_Jenson_Pro_Subhead/otf2ttf.bat
@@ -0,0 +1,9 @@
+@echo off
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-300.otf" "AdobeJensonProSubhead-300.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-300italic.otf" "AdobeJensonProSubhead-300italic.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-400.otf" "AdobeJensonProSubhead-400.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-400italic.otf" "AdobeJensonProSubhead-400italic.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-600.otf" "AdobeJensonProSubhead-600.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-600italic.otf" "AdobeJensonProSubhead-600italic.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-700.otf" "AdobeJensonProSubhead-700.ttf" 
+"C:\Program Files (x86)\FontForgeBuilds\bin\fontforge.exe" -lang=ff -c "Open($1); Generate($2)" "AdobeJensonProSubhead-700italic.otf" "AdobeJensonProSubhead-700italic.ttf" 
